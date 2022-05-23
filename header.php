eca7072a69629667473287b7a72bfa94514f502f
Nonoon971 <94546882+Nonoon971@users.noreply.github.com>
2022-05-23 20:09:48 +0200

    Accueil

---
+++ header.php
@@ -0,0 +1,41 @@
+<header>
+	<h1>Omnes Sport</h1>
+
+
+
+	<div id="nav">
+
+		<ul>
+			<li><a href="#">Accueil</a></li>
+			<li>Tout Parcourir
+				<ul>
+					<li id="test">Activités sportives
+						<ul>
+							<li><a href="musculation.php">Musculation</a></li>
+							<li><a href="fitness.php">Fitness</a></li>
+							<li><a href="biking.php">Biking</a></li>
+							<li><a href="cardio.php">Cardio-Training</a></li>
+							<li><a href="collectif.php">Cours Collectifs</a></li>
+						</ul>
+					</li>
+					<li id="test">Les Sports de compétition
+						<ul>
+							<li><a href="basket.php">Basketball</a></li>
+							<li><a href="football.php">Football</a></li>
+							<li><a href="rugby.php">Rugby</a></li>
+							<li><a href="tennis.php">Tennis</a></li>
+							<li><a href="natation.php">Natation</a></li>
+							<li><a href="plongeon.php">Plongeon</a></li>
+						</ul>
+					</li>
+					<li><a href="salle.php">Salle de sport Omnes</a></li>
+				</ul>
+			</li>
+			<li><a href="recherche.php">Recherche</a></li>
+			<li><a href="rdv.php">Rendez-vous</a></li>
+			<li><a href="compte.php">Votre Compte</a></li>
+
+		</ul>
+
+	</div>
+</header>
