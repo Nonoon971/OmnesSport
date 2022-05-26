c6da74ba50f4c6f2e2f1afc1c8bb28ae1f6d45bb
Nonoon971 <94546882+Nonoon971@users.noreply.github.com>
2022-05-26 17:31:59 +0200

    Update activite.php

--- activite.php
+++ activite.php
@@ -54,6 +54,14 @@
 			text-decoration: none;
 			color: black;
 		}
+
+		#section2
+		{
+			float: left;
+			width: 800px;
+			margin-left: 30px;
+			margin-top: 30px;
+		}
 		#rightcolumn li 
 		{
 			height: 50px;
@@ -67,72 +75,134 @@
 		{
 			opacity: 0.9;
 		}
+		.row.content {height: 450px}
+
+		/* Set gray background color and 100% height */
+		.sidenav {
+			padding-top: 20px;
+			background-color: grey;
+			height: 450%;
+		}
+		.sidenav2 {
+			padding-top: 20px;
+			background-color: grey;
+			float: right;
+			height: 450%;
+		}
 	</style>
 </head>
 <body>
 
 	<?php include('header.php'); ?>
 
-	<div id="rightcolumn">
-		<ul>
-			<li><a href="#muscu">Musculation</a></li>
-			<li><a href="#fitness">Fitness</a></li>
-			<li><a href="#biking">Biking</a></li>
-			<li><a href="#cardio">Cardio-Training</a></li>
-			<li><a href="#collectif">Cours Collectifs</a></li>
-		</ul>
+	
+
+	<div class="container-fluid">    
+		<div class="row content">
+			<div class="col-sm-2 sidenav">
+				<div id="rightcolumn">
+					<ul>
+						<li><a href="#muscu">Musculation</a></li>
+						<li><a href="#fitness">Fitness</a></li>
+						<li><a href="#biking">Biking</a></li>
+						<li><a href="#cardio">Cardio-Training</a></li>
+						<li><a href="#collectif">Cours Collectifs</a></li>
+					</ul>
+				</div>
+			</div>
+			<div id="section2">
+				<div id="section">
+
+					<p>
+						Voici toutes les activités que vous pouvez pratiquer au sein de notre groupe Omnes sport avec nos coachs qui se feront un plaisir de vous accompagner.<br>
+						Choisissez le sport de votre choix selon vos goûts et vos capacités.
+					</p>
+
+					<hr>
+					<div id="muscu">
+						<h2><a href="musculation.php">Musculation</a></h2>
+						<p>
+							Afin de sculpter sa silhouette à son rythme et à son goût. Développer votre masse musculaire.
+						</p>
+						<img src="abdo.jpeg" alt="abdo">
+						<img src="abdo2.jpg">
+						<img src="abdo3.jpg">
+						<p>
+							<br><a style="color: blue;" href="musculation.php">Découvrez notre coach de musculation et ses disponibilités ici</a>
+						</p>
+					</div>
+
+					<hr>
+					<div id="fitness">
+						<h2><a href="fitness.php">Fitness</a></h2>
+						<p>
+							Le fitness est différent de la musculation. Elle regroupe plusieurs activité, le cardio, les étirements, le step etc...<br>
+							Le but est d'à la fois perdre du poids en travaillant tout le corps, son endurance, son cardio, en se musclant etc...
+						</p>
+						<img src="fitness.jpg">
+						<img src="fitness1.jpg">
+						<img src="fitness2.jpg">
+						<p>
+							<br><a style="color: blue;" href="fitness.php">Découvrez notre coach de fitness et ses disponibilités ici</a>
+						</p>
+
+					</div>
+					<hr>
+					<div id="biking">
+						<h2><a href="biking.php">Biking</a></h2>
+						<p>
+							Faisons du vélo en salle ! Un peu comme du cyclisme mais sur un vélo fixe, venez travailler votre cardio et votre physique avec nos vélo de biking. Vous éprouverez les mêmes sensations qu'un vélo de route<br>
+
+						</p>
+						<img src="biking.jpg">
+						<img src="biking1.jpg">
+						<img src="biking3.jpg">
+						<p>
+							<br><a style="color: blue;" href="biking.php">Découvrez notre coach de biking et ses disponibilités ici</a>
+						</p>
+
+					</div>
+
+					<hr>
+					<div id="cardio">
+						<h2><a href="cardio.php">Cardio-Training</a></h2>
+						<p>
+							Le cardio-training, comme son nom l'indique permet de travailler le cardio. En augmentant progressivement l'intensité, on essaye de vous faire contrôler votre fréquence cardiaque. Cela vous permettra d'améliorer la performance de votre coeur.<br>
+
+						</p>
+						<img src="cardio.jpeg">
+						<img src="cardio1.jpg">
+						<img src="cardio2.jpg">
+						<p>
+							<br><a style="color: blue;" href="biking.php">Découvrez notre coach de cardio-training et ses disponibilités ici</a>
+						</p>
+
+					</div>
+
+					<hr>
+					<div id="collectif">
+						<h2><a href="collectif.php">Cours Colelctifs</a></h2>
+						<p>
+							Vous préférez pratiquer en groupe, en famille, entre amis ? Ce cours est fait pour vous ! Zumba, Yoga, step etc... De nombreuses séances et d'activités collectives sont programmées par notre coach pour vous motiver.<br>
+
+						</p>
+						<img src="collectif.jpg">
+						<img src="collectif1.png">
+						<img src="collectif2.jpg">
+						<p>
+							<br><a style="color: blue;" href="biking.php">Découvrez notre coach en charge des cours collectifs et ses disponibilités ici</a>
+						</p>
+
+					</div>
+					<hr>
+
+				</div>
+			</div>
+			<div class="col-sm-2 sidenav2">
+			</div>
+		</div>
 	</div>
-
-	<div id="section">
-		
-		<p>
-			Voici toutes les activités que vous pouvez pratiquer au sein de notre groupe Omnes sport avec nos coachs qui se feront un plaisir de vous accompagner.<br>
-			Choisissez le sport de votre choix selon vos goûts et vos capacités.
-		</p>
-
-		<hr>
-		<div id="muscu">
-			<h2><a href="musculation.php">Musculation</a></h2>
-			<p>
-				Afin de sculpter sa silhouette à son rythme et à son goût. Développer votre masse musculaire.
-			</p>
-			<img src="abdo.jpeg" alt="abdo">
-			<img src="abdo2.jpg">
-			<img src="abdo3.jpg">
-			<p>
-				<br><a style="color: blue;" href="musculation.php">Découvrez notre coach et ses disponibilités ici</a>
-			</p>
-			<br>
-
-		</div>
-		<hr>
-		<div id="fitness">
-			<h2><a href="fitness.php">Fitness</a></h2>
-			<p>
-				Le fitness est différent de la musculation. Elle regroupe plusieurs activité, le cardio, les étirements, le step etc...<br>
-				Le but est d'à la fois perdre du poids en travaillant tout le corps, son endurance, son cardio, en se musclant etc...
-			</p>
-			<img src="fitness.jpg">
-			<img src="fitness1.jpg">
-			<img src="fitness2.jpg">
-			<br><br>
-
-		</div>
-		<hr>
-		<div id="biking">
-			<h2><a href="biking.php">Biking</a></h2>
-			<p>
-				Je continuerai plus tard<br>
-				
-			</p>
-			<img src="fitness.jpg">
-			<img src="fitness1.jpg">
-			<img src="fitness2.jpg">
-			<br><br>
-
-		</div>
-
-	</div>
+		<?php include('footer.php'); ?>
 
 </body>
 </html>
