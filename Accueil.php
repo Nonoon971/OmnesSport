aa47ffab545295d96debed40ebc0325aa1cbbd22
Nonoon971 <94546882+Nonoon971@users.noreply.github.com>
2022-05-27 00:39:22 +0200

    Update Accueil.php

--- Accueil.php
+++ Accueil.php
@@ -37,47 +37,56 @@
 		}
 		#section
 		{
-			margin-left: 20px;
+			margin-left: 50px;
 			font-size: 18px;
 			font-family: Georgia, serif;
 			
 		}
-		#evenement
-		{
-			margin-left: 20px;
-			font-size: 18px;
-		}
+		
 		#section2
 		{
 			float: left;
 			width: 800px;
+			text-align: center;
+		}
+		
+		#evenement
+		{
+			margin-left: 50px;
+			font-size: 18px;
 		}
 		.spacer 
 		{
 			clear: both;
 			overflow: hidden;
 		}
-		
+		footer
+		{
+			background-color: black;
+			color: white;
+			clear: left;
+			text-align: center;
+			padding: 10px;
+			font-size: 0.6em;
+
+		}
 	</style>
-	<style >
+	<style>
 		.carousel-inner img {
 			width: 100%; /* Set width to 100% */
-			margin: 0;
-			min-height:300px;
+			margin: auto;
+			min-height:400px;
 		}
-		.row.content {height: 450px}
-
-		/* Set gray background color and 100% height */
 		.sidenav {
 			padding-top: 20px;
 			background-color: grey;
-			height: 200%;
+			height: 1000px;
 		}
 		.sidenav2 {
 			padding-top: 20px;
 			background-color: grey;
 			float: right;
-			height: 200%;
+			height: 1000px;
 		}
 
 		/* Hide the carousel text when the screen is less than 600 pixels wide */
@@ -85,86 +94,95 @@
 			.carousel-caption {
 				display: none; 
 			}
-		</style>
+		}
+		@media screen and (max-width: 767px) {
+			.sidenav {
+				height: auto;
+				padding: 15px;
+			}
+			.row.content {height:auto;} 
+		}
+	</style>
 
-	</head>
-	<body>
+</head>
+<body>
 
-		<?php include('header.php'); ?>
-		<div class="container-fluid text-center">    
-			<div class="row content">
-				<div class="col-sm-2 sidenav">
+	<?php include('header.php'); ?>
+
+	<div class="row content">
+		<div class="col-sm-2 sidenav">
+		</div>
+		<div id="section2">
+
+			<div id="section">
+
+				<!-- Faire un carrousel -->
+
+				<div id="contenu">
+					<img src="image1Accueil.png" alt="image medecin" height="320" width="400">
 				</div>
-				<div id="section2">
-					<div id="section">
 
-						<!-- Faire un carrousel -->
+				<div id="text">
+					<h2>Omnes Sports</h2>
 
-						<div id="contenu">
-							<img src="image1Accueil.png" alt="image medecin" height="320" width="420">
-						</div>
-
-						<div id="text">
-							<h2>Omnes Sports</h2>
-
-							<p>Bienvenue sur Omnes Sports, la plateforme de consultation sportive en ligne pour la communauté d'Omnes Educations. <br>Ici, les sportifs de tous âges et de tous niveaux confondus pourront prendre rendez-vous, échanger avec un spécialiste de votre choix et le sport de votre choix !</p>
-						</div>
-					</div>
-
-					<div class="spacer"></div>
-
-
-					<div id="evenement">
-						<h2>Evènement de la semaine:</h2>
-						<div id="myCarousel" class="carousel slide" data-ride="carousel">
-							<!-- Indicators -->
-							<ol class="carousel-indicators">
-								<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-								<li data-target="#myCarousel" data-slide-to="1"></li>
-							</ol>
-
-							<!-- Wrapper for slides -->
-							<div class="carousel-inner" role="listbox">
-								<div class="item active">
-									<img src="evenement1.png" alt="Image" height="300" width="300">
-									<div class="carousel-caption">
-										<h3>World Cup FIBA</h3>
-										<p>Women's basketball world cup.</p>
-									</div>      
-								</div>
-
-								<div class="item">
-									<img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="Image">
-									<div class="carousel-caption">
-										<h3>More Sell $</h3>
-										<p>Lorem ipsum...</p>
-									</div>      
-								</div>
-							</div>
-
-							<!-- Left and right controls -->
-							<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-								<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-								<span class="sr-only">Previous</span>
-							</a>
-							<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-								<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-								<span class="sr-only">Next</span>
-							</a>
-						</div>
-						<p>Vous trouverez ici les évènements important de la semaine au sein de notre groupe Omnes mais aussi au niveau du sport mondiale.</p>
-					</div>
-				</div>
-				<div class="col-sm-2 sidenav2">
+					<p>Bienvenue sur Omnes Sports, la plateforme de consultation sportive en ligne pour la communauté d'Omnes Educations. <br>Ici, les sportifs de tous âges et de tous niveaux confondus pourront prendre rendez-vous, échanger avec un spécialiste de votre choix et le sport de votre choix !</p>
 				</div>
 			</div>
+
+			<div class="spacer"></div>
+
+
+			<div id="evenement">
+				<h2>Evènement de la semaine:</h2>
+				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						<li data-target="#myCarousel" data-slide-to="1"></li>
+					</ol>
+
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner" role="listbox">
+						<div class="item active">
+							<img src="evenement1.png" alt="Image">
+							<div class="carousel-caption">
+								<h3>Word Cup 2022 FIBA</h3>
+								<p>Women's basketball world cup</p>
+							</div>      
+						</div>
+
+						<div class="item">
+							<img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="Image">
+							<div class="carousel-caption">
+								<h3>More Sell $</h3>
+								<p>Lorem ipsum...</p>
+							</div>      
+						</div>
+					</div>
+
+					<!-- Left and right controls -->
+					<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+				<p>Vous trouverez ici les évènements important de la semaine au sein de notre groupe Omnes mais aussi au niveau du sport mondiale.</p>
+			</div>
 		</div>
+		<div class="col-sm-2 sidenav2">
+		</div>
+	</div>
 
 
-		<div class="spacer"></div>
+	<div class="spacer"></div>
 
-		<?php include('footer.php'); ?>
+	<?php include 'footer.php';  ?>
 
-		
+
+
 </body>
 </html>
