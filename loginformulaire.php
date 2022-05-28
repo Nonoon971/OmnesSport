a9ab550b9a6117f24b71d393f97441e1bdca2ce7
Nonoon971 <94546882+Nonoon971@users.noreply.github.com>
2022-05-28 19:48:46 +0200

    Amélioration du compte selon type d'user

--- loginformulaire.php
+++ loginformulaire.php
@@ -17,10 +17,10 @@ include'login.php'
 					</div>
 
 					
-						<input type="submit" class="btn btn-primary btn-lg" value="Se connecter" name="submit">
+					<input type="submit" class="btn btn-primary btn-lg" value="Se connecter" name="submit">
 					
 					
-						&nbsp;<button type="button" class="btn btn-success btn-lg" onclick="window.location.href = 'formulaire_inscription.php';">S'inscrire</button>
+					&nbsp;<button type="button" class="btn btn-success btn-lg" onclick="window.location.href = 'formulaire_inscription.php';">S'inscrire</button>
 					
 				</table>
 				<span><?php echo $invalid; ?></span>
@@ -32,7 +32,129 @@ include'login.php'
 	<div class="alert alert-success" role="alert">
 		Bonjour et bienvenue sur votre compte <?php echo $_SESSION['prenom'] ."&nbsp;" .$_SESSION['nom']; ?>
 	</div>
+	<!-- Si c le client qui se connecte on affiche ceci -->
+	<?php if($_SESSION['type'] == 'Client'): ?>
 
-<?php endif; ?>
+		<div id="infoCompte" style="border: 2px solid black; width: 400px; color: black; font-size: 18px; margin-left: 30px;">
+			
+			<h3><b>Voici les informations de votre compte</b></h3>
+
+			<p>
+				<b>Nom: </b>  <?php echo  $_SESSION['nom']; ?><br>
+				<b>Prénom: </b> <?php echo  $_SESSION['prenom']; ?><br>
+				<b>Adresse: </b> <?php echo  $_SESSION['Adresse']; ?><br>
+				<b>Email: </b> <?php echo $_SESSION['Email']; ?> <br>
+				<b>Téléphone</b> <?php echo $_SESSION['telephone']; ?><br>
+			</p>
+			<p>Si une de ses informations est incorrecte veuillez contacter notre service en ligne</p>
+			
+		</div>
+		<br><form action="" method="post"><input style="margin-left: 30px;" type="submit" class="btn btn-primary btn-lg" name="deconnexion" value="Se déconnecter"></form>
+
+		<?php  
+			if(isset($_POST['deconnexion'])) //S'il clique sur le boutton déconnexion on le déconnecte
+			{
+				$conn=mysqli_connect("localhost","root","");
+				$db = mysqli_select_db($conn,"omnessport");
+				$EML = $_SESSION['Email'];
+				$sql = mysqli_query($conn, "UPDATE `utilisateur` SET `Online` = 'false' WHERE Email = '$EML'");
+
+				session_destroy();
+				header("location: Accueil.php");
+
+			}
+		?>
+		<!-- Si c le coach qui se connecte on affiche ceci -->
+	<?php elseif($_SESSION['type'] == 'coach'): ?>
+
+
+		<div id="infoCompte" style="border: 2px solid black; width: 400px; color: black; font-size: 18px; margin-left: 30px; background-color: lightblue;">
+			
+			<h3><b>Voici les informations de votre compte</b></h3>
+
+			<p>
+				<b>Nom: </b>  <?php echo  $_SESSION['nom']; ?><br>
+				<b>Prénom: </b> <?php echo  $_SESSION['prenom']; ?><br>
+				<b>Adresse: </b> <?php echo  $_SESSION['Adresse']; ?><br>
+				<b>Email: </b> <?php echo $_SESSION['Email']; ?> <br>
+				<b>Téléphone</b> <?php echo $_SESSION['telephone']; ?><br>
+			</p>
+			<p>Si une de ses informations est incorrecte veuillez contacter notre service en ligne</p>
+			
+		</div>
+		<br>
+		<form action="" method="post">
+			<input style="margin-left: 30px;" type="submit" class="btn btn-primary btn-lg" name="deconnexion" value="Se déconnecter">
+			<input style="margin-left: 30px;" type="submit" class="btn btn-danger btn-lg" name="chatroom" value="Rejoindre le chat">
+		</form>
+
+		<?php  
+			if(isset($_POST['deconnexion'])) //S'il clique sur le boutton déconnexion on le déconnecte
+			{
+				$conn=mysqli_connect("localhost","root","");
+				$db = mysqli_select_db($conn,"omnessport");
+				$EML = $_SESSION['Email'];
+				$sql = mysqli_query($conn, "UPDATE `utilisateur` SET `Online` = 'false' WHERE Email = '$EML'");
+
+				session_destroy();
+				header("location: Accueil.php");
+
+			}
+			else if(isset($_POST['chatroom'])) //S'il clique sur le boutton déconnexion on le déconnecte
+			{
+	
+				header("location: chat.php");
+			}
+		?>
+
+		<!-- Si c l'administrateur qui se connecte on affiche ceci -->
+		<?php elseif($_SESSION['type'] == 'admin'): ?>
+
+
+		<div id="infoCompte" style="border: 2px solid black; width: 400px; color: black; font-size: 18px; margin-left: 30px; background-color: lightblue;">
+			
+			<h3><b>Voici les informations de votre compte</b></h3>
+
+			<p>
+				<b>Nom: </b>  <?php echo  $_SESSION['nom']; ?><br>
+				<b>Prénom: </b> <?php echo  $_SESSION['prenom']; ?><br>
+				<b>Adresse: </b> <?php echo  $_SESSION['Adresse']; ?><br>
+				<b>Email: </b> <?php echo $_SESSION['Email']; ?> <br>
+				<b>Téléphone</b> <?php echo $_SESSION['telephone']; ?><br>
+			</p>
+			<p>Si une de ses informations est incorrecte veuillez contacter notre service en ligne</p>
+			
+		</div>
+		<br>
+		<form action="" method="post">
+			<input style="margin-left: 30px;" type="submit" class="btn btn-primary btn-lg" name="deconnexion" value="Se déconnecter">
+			<input style="margin-left: 30px;" type="submit" class="btn btn-success btn-lg" name="createCV" value="Ajouter un CV de coach">
+			<input style="margin-left: 30px;" type="submit" class="btn btn-danger btn-lg" name="suppression" value="Ajouter ou supprimer un coach">
+		</form>
+
+		<?php  
+			if(isset($_POST['deconnexion'])) //S'il clique sur le boutton déconnexion on le déconnecte
+			{
+				$conn=mysqli_connect("localhost","root","");
+				$db = mysqli_select_db($conn,"omnessport");
+				$EML = $_SESSION['Email'];
+				$sql = mysqli_query($conn, "UPDATE `utilisateur` SET `Online` = 'false' WHERE Email = '$EML'");
+
+				session_destroy();
+				header("location: Accueil.php");
+
+			}
+			else if(isset($_POST['createCV'])) //S'il clique sur le boutton déconnexion on le déconnecte
+			{
+				header("location: Accueil.php"); // On redirige vers la page de création
+			}
+			else if(isset($_POST['suppression'])) //S'il clique sur le boutton déconnexion on le déconnecte
+			{
+				header("location: Accueil.php"); // On redirige vers la page de suppression
+			}
+		?>
+		<?php endif; ?>
+
+	<?php endif; ?>
 
 
