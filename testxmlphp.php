9c62164d16d91a50e03842cd35922cde449d4dee
NoichiFourbe <pierrick.takezawa@gmail.com>
2022-05-29 00:31:41 +0200

    cvvendredi

--- testxmlphp.php
+++ testxmlphp.php
@@ -6,33 +6,14 @@
 	//append child to parent using appendChild()*/
 	$proc = new XSLTProcessor;
 	$xml = new DOMDocument;
-	$xml->load('zebi.xml');
+	$xml->load('pierricktakezawa.xml');
 	$xsl = new DOMDocument;
 	$xsl->load('cv.xsl');
-	$proc->importStyleSheet($xsl); // attachement des règles xsl
+	$proc->importStyleSheet($xsl); 
 	if($html= $proc->transformToXML($xml)){
 		echo $html;
 	}else{
 		trigger_error('La transformation XSL à échouée.', E_USER_ERROR);
 	}
-
-
-
-	/*$books=$xml->createElement("books");
-	$xml->appendChild($books);
-	//parent->appendChild(child);
-
-	$book = $xml->createElement("book");
-	$book->setAttribute("id",1);
-	$books->appendChild($book);
-
-	$name = $xml->createElement("name","java");
-	$book->appendChild($name);
-	$price = $xml->createElement("price","200");
-	$book->appendChild($price);
-
-	echo "<xmp>" . $xml->saveXML()."</xmp>";
-	$xml->save("zebi.xml") or die("Error, Unable to create XML file");*/
-
 
 ?>
