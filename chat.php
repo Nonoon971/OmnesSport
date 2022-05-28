29048551f16b95aa14e0605c49fba1cd36ecf10a
Nonoon971 <94546882+Nonoon971@users.noreply.github.com>
2022-05-28 23:06:56 +0200

    chatroom

--- chat.php
+++ chat.php
@@ -4,7 +4,7 @@ if (isset($_GET['logout'])){
 
  //Message de sortie simple
  $logout_message = "<div class='msgln'><span class='left-info'>User <b class='user-name-left'>" .
- $_SESSION['nom'] . "</b> a quitté la session de chat.</span><br></div>";
+ $_SESSION['prenom']. "&nbsp;" .$_SESSION['nom'] . "</b> a quitté la session de chat.</span><br></div>";
 
 
  $myfile = fopen(__DIR__ . "/log.html", "a") or die("Impossible d'ouvrir le fichier!" . __DIR__ . "/log.html");
@@ -18,7 +18,7 @@ if (isset($_GET['logout'])){
 <html>
 <head>
  <meta charset="utf-8" />
- <title>Exemple Chat Texto</title>
+ <title>Chat OmnesSport</title>
  <link rel="stylesheet" href="style.css" />
 </head>
 <body id="chatPHP">
@@ -30,7 +30,7 @@ if (isset($_GET['logout'])){
      ?>
      <div id="wrapper">
          <div id="menu">
-            <p class="welcome">Bienvenue, <b><?php echo $_SESSION['nom']; ?></b></p>
+            <p class="welcome">Bienvenue, <b><?php echo $_SESSION['prenom']. "&nbsp;" .$_SESSION['nom']; ?></b></p>
             <p class="logout"><a id="exit" href="#">Quitter la conversation</a></p>
         </div>
         <div id="chatbox">
